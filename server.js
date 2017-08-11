a91445c85d90249f2a141838d49a5f29bd2fc39c
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-11 10:49:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-  res.send(createTemplate(article[articleName]));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/Article-two', function (req, res) {
