dc81ad0f5e8a5a63fec9175aa420c603df975a45
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-13 11:38:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,7 @@ console.log('Loaded!');
 var element= document.getElementById("main-text");
 element.innerHTML="My new html Page";
 // Move the image
-var img= document.getElementById('madi');
-
+var img= document.getElementById("madi");
 var marginLeft = 0;
 function moveRight()
 {
