006631fcd09bd9dbc887f25dbd9305b8505888a3
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-13 12:24:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,6 +111,12 @@ app.get('/Article-three', function (req, res) {
   res.send(createTemplate(articleThree));
 });*/
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter+1;
+    res.send(counter.toString());
+})
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
