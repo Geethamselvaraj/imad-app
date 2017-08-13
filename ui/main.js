1641f783d70f66725d1f3a29f0e711dbcea9d337
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-13 11:49:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,15 +7,10 @@ var img= document.getElementById("madi");
 var marginLeft = 0;
 function moveRight()
 {
-    console.log("Inside moveright fn :"+marginLeft);
     marignLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + "px";
-    console.log("moveright :"+marginLeft);
 }
 img.onclick = function()
 {
-    console.log("insert onclick");
     var interval= setInterval(moveRight,50);
-    console.log("interval :"+interval);
 };
-console.log('loaded img');
