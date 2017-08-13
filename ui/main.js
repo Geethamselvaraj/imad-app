0ab512361f2be68d150d2dd6daee9def9011a62c
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-13 10:43:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 // adding element
-var element= document.getEleme B I("main-text");
-element.inser    L="My new html Page";
+var element= document.getElementById("main-text");
+element.insertHTML="My new html Page";
