88a793ac2c5b78c467360b773c0568cc7be1499c
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-13 11:12:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,9 @@ var element= document.getElementById("main-text");
 element.innerHTML="My new html Page";
 // Move the image
 
-var img = document.getElementById("madi");
-img.onclick = function(){
+//var img = document.getElementById("madi");
+//img.onclick = function(){
     //var interval = setinterval + "1";
     //interval = interval + "px";
-    img.style.marginleft = "100px";
-};
+//    img.style.marginleft = "100px";
+//};
