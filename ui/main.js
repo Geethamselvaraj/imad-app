f7d08a4ded1964515dfc12db8d7161d793430189
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-15 14:41:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,10 +15,10 @@ img.onclick = function()
     var interval= setInterval(moveRight,50);
 };*/
 // Counter code
-/*var button = document.getElementById("counter"); 
+var button = document.getElementById("counter"); 
 button.onclick = function(){
     // Create a request
-    var request = new XMLHttpRequest();
+   /* var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readystate === XMLHttpRequest.DONE){
             if(request.status === 200){
