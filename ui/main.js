0cf07ee99d61a7cae445d5e62046069e19fc12a6
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-15 12:17:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ img.onclick = function()
     var interval= setInterval(moveRight,50);
 };*/
 // Counter code
-var button = document.getElementById("counter"); 
+/*var button = document.getElementById("counter"); 
 button.onclick = function(){
     // Create a request
     var request = new XMLHttpRequest();
@@ -27,14 +27,33 @@ button.onclick = function(){
                 span.innerHTML = counter.toString();
             }
         }
-    };
+    };*/
+    //Creat a request object
+var request = new XMLHttpRequest();
+
+//Capture the response and state and store it in a varible
+request.onreadystatechange = function () {
+  if (request.readyState === XMLHttpRequest.DONE) {
+      //Take some action
+      if (request.status === 200) {
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHtml = counter.toString();
+      }
+  }
+  // Not done yet
+};
+
+// Make the request
+request.open('GET', 'http://http://prasannageetha.imad.hasura-app.io/counter', true);
+request.send(null);
     // Make a request
     //var counter = 0;
     
     //counter = counter+1;
     
-    request.open('GET','http://http://prasannageetha.imad.hasura-app.io/counter',true);
-    request.send(null);
+    //request.open('GET','http://http://prasannageetha.imad.hasura-app.io/counter',true);
+    //request.send(null);
         //var span = document.getElementById("count");
         //span.innerHTML = counter.toString();
 };
