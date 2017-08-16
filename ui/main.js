79bbe169bc0f62c411f88237afb4307b60a2a6a9
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-16 11:01:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,9 +32,9 @@ button.onclick = function(){
     };
     var submit = document.getElementById('submit_btn');
     submit.onclick=function(){
-         var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE)
+        if(request.readyState === XMLHttpRequest.DONE)
         {
             if(request.status === 200){
                 var names = request.responseText;
