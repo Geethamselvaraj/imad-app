a3e6c57a6875a9b574283eca269f95aa48c6e3a9
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-16 10:51:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -65,12 +65,10 @@ button.onclick = function(){
         request.send(null);
         // Make a request
         //var counter = 0;
-        
         //counter = counter+1;
-        
         //request.open('GET','http://http://prasannageetha.imad.hasura-app.io/counter',true);
         //request.send(null);
-            //var span = document.getElementById("count");
-            //span.innerHTML = counter.toString();
+        //var span = document.getElementById("count");
+        //span.innerHTML = counter.toString();
     //};
 };
