4337ac3706858556f1e53cab960eea31dd16fb07
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-16 10:59:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,42 +31,29 @@ button.onclick = function(){
         request.send(null);
     };
     var submit = document.getElementById('submit_btn');
-    submit.onclick = function(){
-    //Creat a request object
-    var request = new XMLHttpRequest();
-
-    //Capture the response and state and store it in a varible
-    request.onreadystatechange = function () {
-    if (request.readyState === XMLHttpRequest.DONE) {
-      //Take some action
-      if (request.status === 200) {
-            //var names = ['name1','name2','name3','name4'];
-            var names = request.responseText;
-            alert('names : '+names);
-            names = JSON.parse(names);
-            var list = '';
-            for (i=0;i<names.length;i++){
-                alert('name[i] : '+names[i]);
-                list += '<li>' + names[i] + '</li>';
+    submit.onclick=function(){
+         var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>' + names[i] + '</li>';
             }  
-        var ul = document.getElementById('namelist');
-        alert('list :'+list);
-        ul.innerHTML = list;
-          /*var counter = request.responseText;
-          var span = document.getElementById('count');
-          span.innerHTML = counter.toString();*/
-      }
-  }
-  // Not done yet
-};
+    var ul = document.getElementById('namelist');
+    ul.innerHTML=list;
 
-        // Make the request
-        //request.open('GET', 'http://prasannageetha.imad.hasura-app.io/counter', true);
-        var nameInput = document.getElementById('name');
-        name = nameInput.value;
-        alert("name :"+name);
-        request.open('GET', 'http://prasannageetha.imad.hasura-app.io/submit-name?name=' + name, true);
-        request.send(null);
+            }
+        }
+    };
+    var nameinput = document.getElementById('name');
+    var name = nameinput.value;
+
+    request.open('GET','http://venisavitri.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);   
         // Make a request
         //var counter = 0;
         //counter = counter+1;
