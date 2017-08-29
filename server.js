a03ddf95e65a81c20a794bf5b3f95ac1678b9ed5
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-29 14:22:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,13 +159,14 @@ app.post('/create-user', function (req, res) {
 });
 
 app.post('/login', function (req, res){
+    console.log('----*----');
     var username = req.body.username;
     var password = req.body.password;
     console.log('----0----');
     pool.query('select * from "user" where username=$1',[username],function(err,result){
         if(err){
              console.log('--1---');
-            //res.status(500).send(err.toString());
+            res.status(500).send(err.toString());
         }else {
             if(result.rows.length === 0)
         {
