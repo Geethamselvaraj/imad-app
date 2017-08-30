8d0d0a6e20e5a98a82edfdf45e80946e46b27a0a
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-30 11:13:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -110,7 +110,7 @@ function loadLoginForm () {
         console.log(password);
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
+        request.send(JSON.stringify({"username": username, "password": password}));  
         register.value = 'Registering...';
     
     };
