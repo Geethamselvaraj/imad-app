8cc9664878f61b1a01b23d9475c0883148b07ef4
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-30 10:14:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret:'someRandomSecretValue',
-    cookie: { maxAge: 1000*60*60*24*30}
+    //cookie: { maxAge: 1000*60*60*24*30}
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }))
 
 /*var articles = {
