8885d1789b694d8fbab8c15eac55fae898916357
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-30 10:48:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -201,6 +201,11 @@ app.get('/check-login', function (req, res) {
     }
 });
 
+app.get('/logout', function (req, res) {
+    delete req.session.auth;
+    res.send('Logged Out');
+});
+
 var pool = new Pool(config);
 //alert("link :"+ user +database + host + port + password);
 app.get('/test-dp', function (req, res) {
