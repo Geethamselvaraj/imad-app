0cd32d8ca20d33ccb6b15bd78f7214aa0665fdff
Geethamselvaraj <30729390+Geethamselvaraj@users.noreply.github.com>
2017-08-30 10:30:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ console.log('Loaded!');
     console.log(username);
     console.log(password);
     request.open('POST','http://prasannageetha.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-Type', 'application/JSON');
+    request.setRequestHeader('Content-Type','application/json');
     //request.send(JSON.stringify({"username":username, "password":password})); 
     request.send(JSON.stringify({'username':username,'password':password}));
 };
